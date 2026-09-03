a6353586edfb85c9759ff95c20636b07b0a48b1c
Florian Hartmann <florian.hartmann@tenhil.de>
2026-09-03 11:59:06 +0200

    s3utils: add delete_unreferenced_files util

--- src/baumbelt/django/s3utils/utils.py
+++ src/baumbelt/django/s3utils/utils.py
@@ -1,16 +1,23 @@
 import contextlib
+import logging
 from collections import defaultdict
 from datetime import datetime
 from typing import Iterable, TypeVar, TypedDict
+
+import boto3
+from django.db.models import FileField
+from storages.backends.s3 import S3Storage
 
 try:
     from tqdm import tqdm
 except ImportError:
     tqdm = None
 
+logger = logging.getLogger(__name__)
 
 FILE_MARKER = "<files>"
 CleanName = TypeVar("CleanName", bound=str)
+_DELETE_BATCH_SIZE = 1000
 
 
 class BucketItem(TypedDict):
@@ -117,3 +124,103 @@ def wait_for_tasks(futures: list, desc):
             if pbar:
                 pbar.update(1)
     return exceptions
+
+
+def _fetch_all_file_keys(s3_client, bucket: str, prefix: str) -> set[str]:
+    logger.debug(f"listing files in bucket '{bucket}' with prefix '{prefix}' ..")
+    file_keys: set[str] = set()
+    marker = ""
+    done = False
+    while not done:
+        response: dict = s3_client.list_objects(Bucket=bucket, Prefix=prefix, MaxKeys=_DELETE_BATCH_SIZE, Marker=marker)
+        files: list[dict] = response.get("Contents", [])
+        file_keys.update(file["Key"] for file in files)
+        if response["IsTruncated"]:
+            logger.debug(f"listed {len(files)} files, but there's more ..")
+            marker = files[-1]["Key"]
+        else:
+            logger.debug(f"listed {len(files)} files, done")
+            done = True
+
+    return file_keys
+
+
+def _delete_file_keys(s3_client, bucket: str, file_keys: list[str], dry_run: bool) -> tuple[int, int]:
+    deleted_count, error_count = 0, 0
+    for start_idx in range(0, len(file_keys), _DELETE_BATCH_SIZE):
+        file_key_batch = file_keys[start_idx : start_idx + _DELETE_BATCH_SIZE]
+        payload = [{"Key": key} for key in file_key_batch]
+        if dry_run:
+            logger.debug(f"would delete batch of {len(payload)} files (if not in a dry-run)")
+            deleted_count += len(payload)
+            continue
+
+        logger.debug(f"deleting batch of {len(payload)} files ..")
+        response: dict = s3_client.delete_objects(
+            Bucket=bucket,
+            Delete={"Objects": payload},
+        )
+        if deletions := response.get("Deleted"):
+            deleted_count += len(deletions)
+        if errors := response.get("Errors"):
+            error_count += len(errors)
+            for err in errors:
+                logger.warning(f"deletion failed: {err}")
+
+    return deleted_count, error_count
+
+
+def delete_unreferenced_files(field: FileField, filename_prefix: str, dry_run: bool = False) -> None:
+    model = field.model
+    verbose_name = f"{model.__name__}.{field.name}"
+    logger.debug(f"going to delete files that are no longer referenced by {verbose_name}")
+
+    if dry_run:
+        logger.debug("no worries, this is just a dry run")
+
+    storage = field.storage
+    if not isinstance(storage, S3Storage):
+        logger.warning(f"storage of {verbose_name} is not an S3Storage, but {type(storage)}, won't delete anything")
+        return
+
+    used_filenames = set(model.objects.values_list(field.name, flat=True))
+    logger.debug(f"{len(used_filenames)} files are referenced in the db and won't be deleted")
+
+    s3_client = boto3.client(service_name="s3")
+
+    # if it's an S3Storage, it must be a PublicMediaStorage or PrivateMediaStorage (from s3utils)
+    # so we can expect it to have specific properties
+    bucket = storage.bucket_name
+    s3_prefix = f"{storage.location}/"
+
+    s3_prefix_length = len(s3_prefix)
+    full_prefix = f"{s3_prefix}{filename_prefix}"
+
+    all_file_keys = _fetch_all_file_keys(s3_client, bucket, full_prefix)
+    logger.debug(f"fetched {len(all_file_keys)} file keys in total")
+
+    file_keys_to_delete, file_keys_to_keep = [], []
+    for key in all_file_keys:
+        filename = key[s3_prefix_length:]
+        if filename in used_filenames:
+            file_keys_to_keep.append(key)
+        else:
+            file_keys_to_delete.append(key)
+
+    logger.debug(
+        f"going to delete {len(file_keys_to_delete)} unused files from S3 "
+        f"({len(file_keys_to_keep)} files will be left untouched)"
+    )
+
+    deleted_count, error_count = _delete_file_keys(s3_client, bucket, file_keys_to_delete, dry_run)
+
+    if error_count:
+        raise AssertionError(f"could not delete {error_count} files from S3")
+
+    if deleted_count != (expected := len(file_keys_to_delete)):
+        raise AssertionError(f"{deleted_count} files were deleted from S3, but we expected {expected}")
+
+    if dry_run:
+        logger.info(f"would have deleted {deleted_count} files from S3 (if not in a dry-run)")
+    else:
+        logger.info(f"successfully deleted {deleted_count} files from S3")
